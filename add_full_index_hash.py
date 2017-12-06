55286b2f7f77d38747c80a52cd71c9ba9b4338e1
CHelfgott <chelfgott@gmail.com>
2017-12-06 12:10:36 -0500

    Made it idempotent, fixed issue with numeric fields.

--- add_full_index_hash.py
+++ add_full_index_hash.py
@@ -17,6 +17,7 @@ Intended to be run on the PSQL machine.
 # join on that one column alone and don't need the map.
 
 import getpass
+import re
 from sqlalchemy import create_engine
 
 # Index columns are identified by having one of these substrings.
@@ -33,6 +34,13 @@ def main():
   pg_conn_str = 'postgresql://' + pg_login + '@localhost:5432/dhs_data'
   engine = create_engine(pg_conn_str, echo=False, paramstyle='format')
   
+  has_full_index_query = (
+      'SELECT table_name '
+      'FROM information_schema.columns '
+      'WHERE table_name ILIKE \'DHS_%%\' '
+      'AND column_name = \'full_index_hash\' '
+      'ORDER BY 1 ASC')
+  
   get_index_columns_query = (
       'SELECT table_name, column_name '
       'FROM information_schema.columns '
@@ -42,20 +50,29 @@ def main():
   get_index_columns_query += '%%\') ORDER BY 1,2 ASC'
 
   table_queries = {}
+  done_tables = set()
   
 #  print(get_index_columns_query)
   with engine.connect() as con:
+    done = con.execute(has_full_index_query)
+    for row in done:
+      done_tables.add(row['table_name'])
+    
     res = con.execute(get_index_columns_query)
     for row in res:
       tname = row['table_name']
+      if tname in done_tables: continue
+      cname = '\"' + row['column_name'] + '\"'
+      if re.search("number", cname, re.IGNORECASE):
+        cname += '::text'
+    
       if not tname in table_queries:
         table_queries[tname] = (
             'UPDATE \"' + tname + '\" '
-            'SET full_index_hash = (\'x\'||substr(md5('
-            '\"' + row['column_name'] + '\"'
+            'SET full_index_hash = (\'x\'||substr(md5(' + cname
         )
       else:
-        table_queries[tname] += '||\"' + row['column_name'] + '\"'
+        table_queries[tname] += '||' + cname
         
   
     for tname in table_queries:
